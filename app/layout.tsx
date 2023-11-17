f69d29fd4f1d8013129f0acd2848af19c0b53db1
mc.hei <mc.hei@Outlook.com>
2023-11-17 19:33:48 +1030

    Inital Car_showcase

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from 'next';
+import { Navbar } from '@/components';
+import { Footer } from '@/components';
+
+import './globals.css'
+
+export const metadata: Metadata = {
+  title: 'Car Hub',
+  description: 'Discover the best cars.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className="relative">
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  )
+}
---
+++ components/index.ts
@@ -0,0 +1,24 @@
+import Hero from './Hero';
+import Navbar from './Navbar';
+import Footer from './Footer';
+import CustomFilter from './CustomFilter';
+import SearchBar from './SearchBar';
+import SearchManufacturer from './SearchManufacturer';
+import CustomButton from "./CustomButton";
+import CarCard from './CarCard';
+import ShowMore from './ShowMore';
+import CarDetails from './CarDetails';
+
+
+export {
+    Hero,
+    CustomButton,
+    Navbar,
+    Footer,
+    CustomFilter,
+    SearchBar,
+    SearchManufacturer,
+    CarCard,
+    ShowMore,
+    CarDetails
+}
